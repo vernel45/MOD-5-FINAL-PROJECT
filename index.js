017e98b0b294ff47632d186b4bd247a1ef81886c
Sam Verneuille <samvernel268@gmail.com>
2026-09-09 14:06:39 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-async function getMovies() {
-  const response = await fetch("https://www.omdbapi.com/?apikey=2ade860f&s=marvel");
-  const data = await response.json();
-  console.log(data);
-}
+const results = document.getElementById("results");
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
 
-getMovies();
+searchButton.addEventListener("click", () => {
+  getMovies(searchInput.value);
+});
+
+async function getMovies(searchTerm) {
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=2ade860f&s=${searchTerm}`,
+  );
+
+  const data = await response.json();
+  console.log(data.Search);
+
+  const movies = data.Search;
+
+  results.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie");
+
+    const poster =
+      movie.Poster && movie.Poster !== "N/A"
+        ? movie.Poster
+        : "https://via.placeholder.com/300x450?text=No+Image";
+        console.log(movie.Title, poster);
+
+    movieCard.innerHTML = `
+<img 
+src="${poster}"
+  alt="${movie.Title}"
+  onerror="this.onerror=null; this.src='https://placehold.co/300x450?text=No+Image';"
+/>
+<h3>${movie.Title}</h3>
+<p>${movie.Year}</p>
+  `;
+
+    results.appendChild(movieCard);
+  });
+}
