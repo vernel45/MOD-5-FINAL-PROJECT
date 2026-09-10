676bfddb5849676727b6ced061fbc72976c3f3b3
Sam Verneuille <samvernel268@gmail.com>
2026-09-09 22:20:48 -0400

    updated js and html

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const results = document.getElementById("results");
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
+const nameWrapper = document.querySelector(".searchName");
 
 searchButton.addEventListener("click", () => {
-    const searchTerm = searchInput.value.trim();
+  const searchTerm = searchInput.value.trim();
 
-if (!searchTerm) {
-  results.innerHTML = "<h2>Please enter a movie title.</h2>";
-  return;
-}
+  if (!searchTerm) {
+    results.innerHTML = "<h2>Please enter a movie title.</h2>";
+    return;
+  }
 
-getMovies(searchTerm);
- });
+  getMovies(searchTerm);
+  nameWrapper.innerHTML = searchTerm;
+});
 
- searchInput.addEventListener("keydown", (event) => {
- if (event.key === "Enter") {
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     const searchTerm = searchInput.value.trim();
     if (!searchTerm) {
-        results.innerHTML = "<h2> Please add a movie title</h2>";
+      results.innerHTML = "<h2> Please add a movie title</h2>";
     }
     getMovies(searchTerm);
- }
+  }
 });
 
 async function getMovies(searchTerm) {
@@ -29,36 +31,32 @@ async function getMovies(searchTerm) {
   );
 
   const data = await response.json();
-if (data.Response === "False") {
-  results.innerHTML = `<h2>${data.Error}</h2>`;
-  return;
-}
+  if (data.Response === "False") {
+    results.innerHTML = `<h2>${data.Error}</h2>`;
+    return;
+  }
   console.log(data);
 
-  const movies = data.Search;
+ const movies = data.Search;
 
-  results.innerHTML = "";
-
-  movies.forEach((movie) => {
-    const movieCard = document.createElement("div");
-    movieCard.classList.add("movie");
-
+results.innerHTML = movies
+  .map((movie) => {
     const poster =
       movie.Poster && movie.Poster !== "N/A"
         ? movie.Poster
-        : "https://via.placeholder.com/300x450?text=No+Image";
-        console.log(movie.Title, poster);
+        : "https://placehold.co/300x450?text=No+Image";
 
-    movieCard.innerHTML = `
-<img 
-src="${poster}"
-  alt="${movie.Title}"
-  onerror="this.onerror=null; this.src='https://placehold.co/300x450?text=No+Image';"
-/>
-<h3>${movie.Title}</h3>
-<p>${movie.Year}</p>
-  `;
-
-    results.appendChild(movieCard);
-  });
+    return `
+      <div class="movie">
+        <img
+          src="${poster}"
+          alt="${movie.Title}"
+          onerror="this.onerror=null; this.src='https://placehold.co/300x450?text=No+Image';"
+        />
+        <h3>${movie.Title}</h3>
+        <h4>${movie.Year}</h4>
+        <button> Learn More</button>
+      </div>
+    `;
+  }).slice(0, 6)  .join("");
 }
